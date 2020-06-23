0b28a09afb7512555d890b0e5a0c815b41ccd4ee
Andres Pirela <andreespirela@outlook.com>
2020-06-23 09:26:53 -0400

    moved to mandarineorg/mandarine-postgres

--- postgres/0.4.2/test_deps.ts
+++
@@ -1,8 +0,0 @@
-export * from "./deps.ts";
-export {
-  assert,
-  assertEquals,
-  assertStrContains,
-  assertThrows,
-  assertThrowsAsync,
-} from "https://deno.land/std@0.51.0/testing/asserts.ts";
