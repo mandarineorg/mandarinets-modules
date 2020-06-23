a805cfc92f3477161319bd14f89b8ba26ef4eaca
Andres Pirela <andreespirela@outlook.com>
2020-06-22 20:45:09 -0400

    no message

---
+++ mandarine-http-dispatcher/deps.ts
@@ -0,0 +1,51 @@
+// Copyright 2018-2020 the oak authors. All rights reserved. MIT license.
+
+// This file contains the external dependencies that oak depends upon
+
+// `std` dependencies
+
+export { equal } from "https://deno.land/std@0.58.0/bytes/mod.ts";
+export { Sha1 } from "https://deno.land/std@0.58.0/hash/sha1.ts";
+export { HmacSha256 } from "https://deno.land/std@0.58.0/hash/sha256.ts";
+export {
+  serve,
+  serveTLS,
+} from "https://deno.land/std@0.58.0/http/server.ts";
+export {
+  Status,
+  STATUS_TEXT,
+} from "https://deno.land/std@0.58.0/http/http_status.ts";
+export { BufReader, BufWriter } from "https://deno.land/std@0.58.0/io/bufio.ts";
+export { copyBytes } from "https://deno.land/std@0.58.0/io/util.ts";
+export {
+  basename,
+  extname,
+  join,
+  isAbsolute,
+  normalize,
+  parse,
+  resolve,
+  sep,
+} from "https://deno.land/std@0.58.0/path/mod.ts";
+export { assert } from "https://deno.land/std@0.58.0/testing/asserts.ts";
+export {
+  acceptable,
+  acceptWebSocket,
+  WebSocket,
+} from "https://deno.land/std@0.58.0/ws/mod.ts";
+
+// 3rd party dependencies
+
+export {
+  contentType,
+  extension,
+  lookup,
+} from "https://deno.land/x/media_types@v2.3.7/mod.ts";
+export {
+  compile,
+  Key,
+  parse as pathParse,
+  ParseOptions,
+  pathToRegexp,
+  TokensToRegexpOptions,
+} from "https://raw.githubusercontent.com/pillarjs/path-to-regexp/v6.1.0/src/index.ts";
